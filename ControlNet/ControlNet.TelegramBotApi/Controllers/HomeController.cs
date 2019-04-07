14846a0f16f13b747fcc8007bb89c778bf9d39a0
Alexey Kukushkin <kuckushckin.alex@yandex.ru>
2019-04-07 11:59:50 +0300

    Created solution and project TelegramBot API

---
+++ ControlNet/ControlNet.TelegramBotApi/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControlNet.TelegramBotApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        #region Methods
+
+        [HttpGet]
+        public string Get() => "Hello! I'm Telegergam Bot API. :)";
+
+        #endregion
+    }
+}
