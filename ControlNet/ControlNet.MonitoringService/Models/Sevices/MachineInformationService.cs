3a544d8efadc0c9569d016706b2c20cb35577a03
Alexey Kukushkin <kuckushckin.alex@yandex.ru>
2019-04-27 19:54:40 +0300

    Added machine information service and rest client service. Included necessary packages in the assembly.

---
+++ ControlNet/ControlNet.MonitoringService/Models/Sevices/MachineInformationService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControlNet.MonitoringService.Models.Sevices
+{
+    public static class MachineInformationService
+    {
+
+        #region Fields
+
+        #endregion
+
+        #region Constructors
+
+        #endregion
+
+        #region Methods
+
+        public static string GetMachineName()
+        {
+            var machineName = Environment.MachineName;
+            return machineName;
+        }
+
+        #endregion
+
+    }
+}
---
+++ ControlNet/ControlNet.MonitoringService/Models/Sevices/RestClientService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControlNet.MonitoringService.Models.Sevices
+{
+    public static class RestClientService
+    {
+        #region Fields
+
+        #endregion
+
+        #region Constructors
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
