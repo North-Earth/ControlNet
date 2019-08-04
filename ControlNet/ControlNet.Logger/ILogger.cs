3eb883ba8a0aacc0982dfe46fafb8b0595a48f80
Alexey Kukushkin <kuckushckin.alex@yandex.ru>
2019-08-04 23:26:30 +0300

    Created Logger service.

---
+++ ControlNet/ControlNet.Logger/ILogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlNet.Logger
+{
+    public interface ILogger
+    {
+        #region Methods
+
+        Task WriteInformationAsync(string message);
+
+        Task WriteWarningAsync(string message);
+
+        Task WriteErrorAsync(string message);
+
+        Task WriteDebugAsync(string message);
+
+        #endregion
+    }
+}
---
+++ ControlNet/ControlNet.Logger/Logger.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ControlNet.Logger
+{
+    public class Logger : ILogger
+    {
+        #region Fields
+
+        private string FileLogPath { get; } = null;
+
+        private enum Status
+        {
+            Information,
+            Warning,
+            Error,
+            Debug
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public Logger() { }
+
+        #endregion
+
+        #region Methods
+        private async Task Write(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task Write(string message, string prefix, ConsoleColor prefixColor)
+        {
+            var dateTime = DateTime.Now.ToString();
+            var log = $" [{dateTime}]: {message}";
+
+            WriteToConsole(log, prefix, prefixColor);
+            await WriteToFileAsync($"{prefix}{log}");
+        }
+
+        private void WriteToConsole(string message, string prefix, ConsoleColor prefixColor)
+        {
+            Console.ForegroundColor = prefixColor;
+            Console.Write(prefix);
+            Console.ResetColor();
+
+            Console.WriteLine(message);
+        }
+
+        private async Task WriteToFileAsync(string message)
+        {
+            var appDir = AppDomain.CurrentDomain.BaseDirectory;
+            var logsDir = $@"{appDir}Logs";
+            if (!Directory.Exists(logsDir))
+                Directory.CreateDirectory(logsDir);
+            var path = $@"{logsDir}\log.txt";
+
+            await File.AppendAllTextAsync(path, message + "\n");
+        }
+
+        public async Task WriteInformationAsync(string message)
+            => await Write(message, Status.Information.ToString(), ConsoleColor.White);
+
+        public async Task WriteWarningAsync(string message)
+            => await Write(message, Status.Warning.ToString(), ConsoleColor.Yellow);
+
+        public async Task WriteErrorAsync(string message)
+            => await Write(message, Status.Error.ToString(), ConsoleColor.Red);
+
+        public async Task WriteDebugAsync(string message)
+            => await Write(message, Status.Debug.ToString(), ConsoleColor.Gray);
+
+        #endregion
+    }
+}
