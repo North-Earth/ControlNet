cd12bfd978e19f503486e01cfb6b2cfdb8e627d5
Alexey Kukushkin <kuckushckin.alex@yandex.ru>
2019-08-04 23:28:43 +0300

    Fix bugs in Resources Service

---
+++ ControlNet/ControlNet.MonitoringService/Models/Sevices/IResourcesService.cs
@@ -0,0 +1,8 @@
+﻿namespace ControlNet.MonitoringService.Models.Sevices
+{
+    public interface IResourcesService
+    {
+        string GetResource(string resourceKey);
+        void SetResource(string resourceKey, string resourceValue);
+    }
+}
--- ControlNet/ControlNet.MonitoringService/Models/Sevices/ResourcesService.cs
+++ ControlNet/ControlNet.MonitoringService/Models/Sevices/ResourcesService.cs
@@ -1,38 +1,66 @@
-﻿using System;
+﻿using ControlNet.Logger;
+using System;
 using System.Configuration;
+using System.IO;
 using System.Linq;
 
 namespace ControlNet.MonitoringService.Models.Sevices
 {
-    public static class ResourcesService
+    public class ResourcesService : IResourcesService
     {
-        public static Configuration Configuration { get; } = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+        #region Fields
+        private Configuration Configuration { get; }
+
+        private ILogger Logger { get; }
+
+        private const string settingsSection = "appSettings";
+
+        #endregion
+
+        #region Constructors
+
+        public ResourcesService(ILogger logger)
+        {
+            string applicationName =
+                Environment.GetCommandLineArgs()[0];
+
+            string path = System.IO.Path.Combine(
+                Environment.CurrentDirectory, applicationName);
+
+            Configuration = ConfigurationManager.OpenExeConfiguration(exePath: path);
+            Logger = logger;
+        }
+
+        #endregion
 
         #region Methods
 
-        public static string GetResource(string resourceKey)
+        public string GetResource(string resourceKey)
         {
             if (Configuration.AppSettings.Settings.AllKeys.Any(k => k == resourceKey))
                 return Configuration.AppSettings.Settings[resourceKey].Value;
-            
+
+            Logger.WriteWarningAsync($"Resource {resourceKey} is not found.");
             return null;
         }
 
-        public static void SetResource(string resourceKey, string resourceValue)
+        public void SetResource(string resourceKey, string resourceValue)
         {
             // Check if not exists key.
             if (!Configuration.AppSettings.Settings.AllKeys.Any(k => k == resourceKey))
             {
                 Configuration.AppSettings.Settings.Add(new KeyValueConfigurationElement(resourceKey, resourceValue));
+                Logger.WriteInformationAsync($"Resource {resourceKey} has been added.");
             }
             else // Make changes.
             {
                 Configuration.AppSettings.Settings[resourceKey].Value = resourceValue;
+                Logger.WriteInformationAsync($"Resource {resourceKey} has been edited.");
             }
 
             // Save to apply changes.
             Configuration.Save(ConfigurationSaveMode.Modified);
-            ConfigurationManager.RefreshSection("appSettings");
+            ConfigurationManager.RefreshSection(settingsSection);
         }
 
         #endregion
