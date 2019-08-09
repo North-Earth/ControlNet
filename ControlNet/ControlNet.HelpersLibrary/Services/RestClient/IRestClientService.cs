c5815d52be4ab9af8a9a6585cae070789bfa4b52
Alexey Kukushkin <kuckushckin.alex@yandex.ru>
2019-08-10 00:45:43 +0300

    Added RestClient to HelperLibrary

---
+++ ControlNet/ControlNet.HelpersLibrary/Services/RestClient/IRestClientService.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+
+namespace ControlNet.HelpersLibrary.Services.RestClient
+{
+    public interface IRestClientService
+    {
+
+        #region Methods
+
+        /// <summary>
+        /// Sends GET-request to Read object.
+        /// </summary>
+        /// <returns></returns>
+        Task SendGetRequestAsync();
+
+        /// <summary>
+        /// Sends POST-request to Create object.
+        /// </summary>
+        /// <returns></returns>
+        Task SendPostRequestAsync();
+
+        /// <summary>
+        /// Sends POST-request to Update/Replace object.
+        /// </summary>
+        /// <returns></returns>
+        Task SendPutRequestAsync();
+
+
+        /// <summary>
+        /// Sends DELETE-request to Delete object.
+        /// </summary>
+        /// <returns></returns>
+        Task SendDeleteRequestAsync();
+
+        #endregion
+    }
+}
---
+++ ControlNet/ControlNet.HelpersLibrary/Services/RestClient/RestClientService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlNet.HelpersLibrary.Services.RestClient
+{
+    public class RestClientService : IRestClientService
+    {
+        #region Fields
+
+        #endregion
+
+        #region Constructors
+
+        public RestClientService() { }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Sends GET-request to Read object.
+        /// </summary>
+        /// <returns></returns>
+        public async Task SendDeleteRequestAsync()
+        {
+            //TODO: Implementation.
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Sends POST-request to Create object.
+        /// </summary>
+        /// <returns></returns>
+        public async Task SendGetRequestAsync()
+        {
+            //TODO: Implementation.
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Sends POST-request to Update/Replace object.
+        /// </summary>
+        /// <returns></returns>
+        public async Task SendPostRequestAsync()
+        {
+            //TODO: Implementation.
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Sends DELETE-request to Delete object.
+        /// </summary>
+        /// <returns></returns>
+        public async Task SendPutRequestAsync()
+        {
+            //TODO: Implementation.
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
